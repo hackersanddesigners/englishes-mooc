422bba830aaf461603374c7b6044db5024a3d2d4
afincato <work@andrefincato.info>
2019-01-25 23:42:23 +0100

    clean up

--- app/views/main.js
+++ app/views/main.js
@@ -13,12 +13,14 @@ function view (state, emit) {
 
   return html`
     <body style="background: url(${ov(page[0].files)[0].url}); background-size: cover" class="vh100 bgpc bgrn">
-      <button class="psf mt2 ${state.toggle ? "mw-bt t0 l50 ttxc md-ttx0 md-r0 md-mgr2 md-la sh-a" : "t0 r0 mr1 md-mr2"} z2 ft-mn py0-25 px0-5 bgc-wh curp" onclick=${toggle}>${state.toggle ? "About Englishes MOOC" : "x"}</button>
+      <main>
+        <button class="psf mt2 ${state.toggle ? "mw-bt t0 l50 ttxc md-ttx0 md-r0 md-mgr2 md-la sh-a" : "t0 r0 mr1 md-mr2"} z2 ft-mn py0-25 px0-5 bgc-wh curp" onclick=${toggle}>${state.toggle ? "About Englishes MOOC" : "x"}</button>
 
-      <div class="pn ${state.toggle ? "dn" : "db mr0-5 ml0-5 sh-a"} mhv-bx os psf t0 mt2 r0 md-mr2 z1 p1 bgc-wh sm-w60 md-w40">
-        <h1 class="ft-bd fs1">${data.content.title}</h1>
-        <div class="copy-w pb1">${raw(md.render(page[0].content.text))}</div>
-      </div>
+        <div class="pn ${state.toggle ? "dn" : "db mr0-5 ml0-5 sh-a"} mhv-bx os psf t0 mt2 r0 md-mr2 z1 p1 bgc-wh sm-w60 md-w40">
+          <h1 class="ft-bd fs1">${data.content.title}</h1>
+          <div class="copy-w pb1">${raw(md.render(page[0].content.text))}</div>
+        </div>
+      </main>
     </body>
   `
 
