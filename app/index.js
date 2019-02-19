30f04c8b74d3a9e6ae008d4eda1b7653e0df8738
afincato <work@andrefincato.info>
2019-02-19 17:22:10 +0100

    building proper router for sub & sub-sub-pages

--- app/index.js
+++ app/index.js
@@ -1,6 +1,7 @@
 var choo = require('choo')
 var html = require('choo/html')
 var devtools = require('choo-devtools')
+var ok = require('object-keys')
 var ov = require('object-values')
 var css = require('sheetify')
 css('./design/index.js')
@@ -12,14 +13,17 @@ app.use(devtools())
 app.use(require('./stores/click'))
 
 var views = {
-  about: require("./views/main"),
+  about: require("./views/about"),
+  course: require("./views/course"),
+  module: require("./views/module"),
+  chapter: require("./views/chapter"),
   hkw: require("./views/preview"),
   signup: require("./views/signup"),
   error: require("./views/notfound")
 }
 
 ov(data.children).map(function (page) {
-  app.route('/' + page.id, (state, emit) => views[page.uid](state, emit))
+  app.route('/' + page.id, (state, emit) => views[page.template](state, emit))
 })
 
 app.route('/', require('./views/main'))
