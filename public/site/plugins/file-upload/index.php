23f49a850de2380d23982859e79c25e1bff4f482
afincato <work@andrefincato.info>
2019-03-09 22:35:15 +0100

    clean up

--- public/site/plugins/file-upload/index.php
+++ public/site/plugins/file-upload/index.php
@@ -5,34 +5,14 @@ Kirby::plugin('mooc/upload', [
     [
       'method' => 'POST',
       'pattern' => 'apiupload',
-      'action'  => function () {
-        if(r::is('POST')) {
-          $r = r::data();
-
-          $data = array(
-            'module' => get('module'),
-            'file' => get('file'),
-            /* 'lastModified' => get('lastModified'), */
-            /* 'name' => get('name'), */
-            /* 'size' => get('size'), */
-            /* 'type' => get('type') */
-          );
-
-          try {
-            $p = site()->children()->findBy('uid', 'course')->children()->create(str::slug(time()), 'assignment', array(
-              'module' => 'module-*',
-              'file' => 'my file',
-            ));
-            echo 'The page has been created';
-
-          } catch(Exception $e) {
-            echo $e->getMessage();
-            echo 'The page could not be created';
-          }
-
-          /* return response::json(json_encode($data)); */
-        }
-
+      'action'  => function (string $path) {
+        return $this->upload(function ($source, $filename) use ($path) {
+          return $this->parent($path)->createFile([
+            'source' => $source,
+            'template' => $this->requestBody('template'),
+            'filename' => $filename
+          ]);
+        });
       }
     ]
   ]
