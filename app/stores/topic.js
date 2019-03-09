f25d710e29022e488043f0e625bbd69208d69649
afincato <work@andrefincato.info>
2019-03-09 22:33:43 +0100

    add correct cat_id

--- app/stores/topic.js
+++ app/stores/topic.js
@@ -1,19 +1,21 @@
 var ok = require('object-keys')
+var ov = require('object-values')
 var xhr = require('xhr')
 var users = require('./users.json')
 
 function topic (state, emitter) {
-  const cat_id = '5'
-
   const user_s = JSON.parse(localStorage.getItem('user_data'))
   if (user_s !== null) {
     const user = ok(users).filter(user => user === user_s.user.username)
 
     emitter.on('topic', () => {
+      const page = ov(state.content).filter(page => page.uri === state.route)[0]
+      const cat_id = page.content.cat_id
+
       xhr({
         method: 'get',
         headers: {'Content-Type': 'multipart/form-data'},
-        url: `https://forum.englishes-mooc.org/c/${ cat_id }.json?api_key=${users[user]}&api_username=${user}`,
+        url: `https://forum.englishes-mooc.org/c/${ cat_id }.json?api_key=${users[user]}&api_username=${ok(users)[0]}`,
         json: true,
       }, function (err, resp, body) {
         if (err) throw err
