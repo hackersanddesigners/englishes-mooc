aef1569a2015cc536acfb48bd8f59e7cc5fc5a2f
afincato <work@andrefincato.info>
2019-03-09 16:34:18 +0100

    add custom icons

--- app/components/statusbar.js
+++ app/components/statusbar.js
@@ -42,17 +42,33 @@ class statusbar extends nc {
         `
 
         function status () {
+          const clocks = {
+            'clock-01': '<svg width="28" height="28"><g stroke="#231F20" stroke-width=".86" fill="none" fill-rule="evenodd"><path d="M14.31 27.55c7.257 0 13.14-5.883 13.14-13.14 0-7.257-5.883-13.14-13.14-13.14-7.257 0-13.14 5.883-13.14 13.14 0 7.257 5.883 13.14 13.14 13.14zM14.31 3.99v11.42M23.53 15.41h-9.65"/></g></svg>',
+            'clock-02': '<svg width="28" height="28"><g transform="translate(1)" stroke="#231F20" stroke-width=".86" fill="none" fill-rule="evenodd"><circle cx="13.31" cy="13.93" r="13.14"/><path d="M5.62 6.74l8.87 7.19M7.97 20.45l6.83-6.82"/></g></svg>',
+            'clock-03': '<svg width="28" height="28"><g transform="translate(1 1)" stroke="#231F20" stroke-width=".86" fill="none" fill-rule="evenodd"><circle cx="13.31" cy="13.3" r="13.14"/><path d="M5.79 20.82l8.07-8.07M7.35 6.23l6.82 6.82"/></g></svg>',
+            'clock-04': '<svg width="28" height="28"><g transform="translate(1)" stroke="#231F20" stroke-width=".86" fill="none" fill-rule="evenodd"><circle cx="13.31" cy="13.66" r="13.14"/><path d="M5.24 7.86l8.07 8.08M19.83 9.42l-6.82 6.82"/></g></svg>'
+          }
+
+          let random_clock = (obj) => {
+            var keys = ok(obj)
+            return obj[keys[ keys.length * Math.random() << 0]];
+          }
+
           if(item.content.status === 'current') {
             return html`
-              <div class="pl1">+</div>
+              <div class="pl1">
+                <svg width="13" height="2"><text transform="translate(-1 -17)" fill="#231F20" fill-rule="evenodd" font-family="Helvetica" font-size="18.58"><tspan x="0" y="18.63">...</tspan></text></svg>
+              </div>
             `
           } else if(item.content.status === 'done') {
             return html`
-              <div class="pl1">x</div>
+              <div class="pl1">
+                <svg width="28" height="20"><g stroke="#231F20" stroke-width=".75" fill="none" fill-rule="evenodd"><path d="M.03 10.48l8.8 8.79M27.2.27l-18.76 19"/></g></svg>
+              </div>
             `
           } else if(item.content.status === 'upcoming') {
             return html`
-              <div class="pl1">...</div>
+              <div class="pl1">${ raw(random_clock(clocks)) }</div>
             `
           }
         }
