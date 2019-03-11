3afa93aefea6ac3311ad7ca280c0146a81ea6f4f
afincato <work@andrefincato.info>
2019-03-11 11:45:37 +0100

    setup forum nc for `/` as well as topic nc for module

--- app/stores/post-pag.js
+++ app/stores/post-pag.js
@@ -9,9 +9,16 @@ function post_pag (state, emitter) {
     const user = ok(users).filter(user => user === user_s.user.username)
 
     emitter.on('post-pag', (post_id, topic_n) => {
-      console.log(post_id, topic_n)
-      const page = ov(state.content).filter(page => page.uri === state.route)[0]
-      const cat_id = page.content.cat_id
+      let page
+      let cat_id
+
+      if (state.route !== '/') {
+        page = ov(state.content).filter(page => page.uri === state.route)[0]
+        cat_id = page.content.cat_id
+      } else {
+        page = ov(state.content).filter(page => page.content.status === 'current')[0]
+        cat_id = page.content.cat_id
+      }
 
       if(state.components.cat !== undefined) {
         const topics = state.components.cat.topic_list.topics
@@ -21,14 +28,26 @@ function post_pag (state, emitter) {
           xhr({
             method: 'get',
             headers: {'Content-Type': 'multipart/form-data'},
-            url: `https://forum.englishes-mooc.org/t/${ topic[0].id }/20.json?api_key=${users[user]}&api_username=${user[0]}`,
+            url: `https://forum.englishes-mooc.org/t/${ topic[0].id }/${ post_id || 20 }.json?api_key=${users[user]}&api_username=${user[0]}`,
             json: true,
           }, function (err, resp, body) {
             if (err) throw err
-            console.log('loading more posts')
             console.log(body)
+            let comp_n = topic_n + '_pag'
+            // state.components.post_pag = body
+            state.components[comp_n] = body
 
-            state.components.post_pag = body
+            const posts = ov(body.post_stream.posts).filter(post => post.user_deleted === false)
+            const post_tot = posts.length -1
+            const post_n_l = posts[post_tot].post_number
+            const stream = ov(body.post_stream.stream)
+            const stream_tot = stream.length -1
+
+            if(post_n_l < stream_tot) {
+              state.components.loadmore = true
+            } else {
+              state.components.loadmore = false
+            }
 
           })
 
@@ -37,6 +56,10 @@ function post_pag (state, emitter) {
         }
       }
 
+    })
+
+    emitter.on('loadmore', () => {
+      emitter.emit('render')
     })
 
   }
