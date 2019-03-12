dceadb28a82d97174124630c5a095b0c4553e0a9
afincato <work@andrefincato.info>
2019-03-12 01:28:16 +0100

    make fetch_topic component

---
+++ app/components/fetch-topic.js
@@ -0,0 +1,80 @@
+var ok = require('object-keys')
+var ov = require('object-values')
+var xhr = require('xhr')
+var users = require('../stores/users.json')
+
+function fetch_topic (state, emitter, page, cat_id) {
+  const user_s = JSON.parse(localStorage.getItem('user_data'))
+  if (user_s !== null) {
+    const user = ok(users).filter(user => user === user_s.user.username)
+
+    state.components.disc_posts = []
+    state.components.todo_posts = []
+
+    xhr({
+      method: 'get',
+      headers: {'Content-Type': 'multipart/form-data'},
+      url: `https://forum.englishes-mooc.org/c/${ cat_id }.json?api_key=${users[user]}&api_username=${user[0]}`,
+      json: true,
+    }, function (err, resp, body) {
+      if (err) throw err
+      // console.log(body)
+      state.components.cat = body
+
+      if(state.components.cat !== undefined) {
+        const topics = state.components.cat.topic_list.topics
+        const disc = topics.filter(tag => tag.tags.includes('discussion-test'))
+        const todo = topics.filter(tag => tag.tags.includes('assignment-test'))
+
+        if (disc.length > 0) {
+          xhr({
+            method: 'get',
+            headers: {'Content-Type': 'multipart/form-data'},
+            url: `https://forum.englishes-mooc.org/t/${ disc[0].id }.json?api_key=${users[user]}&api_username=${user[0]}`,
+            json: true,
+          }, function (err, resp, body) {
+            if (err) throw err
+            // console.log(body)
+            state.components.discussion = body
+
+            let posts = body.post_stream.posts
+            posts.forEach(function (post) {
+              if (ov(state.components.disc_posts).filter(item => item.id !== post.id)) {
+                // console.log(post.id, 'is included already')
+                state.components.disc_posts.push(post)
+              }
+            })
+
+            emitter.emit('render')
+          })
+        }
+
+        if (todo.length > 0) {
+          xhr({
+            method: 'get',
+            headers: {'Content-Type': 'multipart/form-data'},
+            url: `https://forum.englishes-mooc.org/t/${ todo[0].id }.json?api_key=${users[user]}&api_username=${user[0]}`,
+            json: true,
+          }, function (err, resp, body) {
+            if (err) throw err
+            // console.log(body)
+            state.components.assignment = body
+
+            let posts = body.post_stream.posts
+            posts.forEach(function (post) {
+              if (ov(state.components.todo_posts).filter(item => item.id !== post.id)) {
+                state.components.todo_posts.push(post)
+              }
+            })
+
+            emitter.emit('render')
+          })
+        }
+
+      }
+    })
+  }
+
+}
+
+module.exports = fetch_topic
--- app/stores/click.js
+++ app/stores/click.js
@@ -2,6 +2,7 @@ var ok = require('object-keys')
 var ov = require('object-values')
 var xhr = require('xhr')
 var users = require('./users.json')
+var fetch_topic = require('../components/fetch-topic')
 
 function clickhandle (state, emitter) {
   state.sidebar = false
@@ -157,6 +158,20 @@ function clickhandle (state, emitter) {
   emitter.on('msg-posted', () => {
     document.querySelector('.send').value = 'post'
     document.querySelector('textarea').value = ''
+
+    let page
+    let cat_id
+
+    if (state.route !== '/') {
+      page = ov(state.content).filter(page => page.uri === state.route)[0]
+      cat_id = page.content.cat_id
+    } else {
+      page = ov(state.content).filter(page => page.content.status === 'current')[0]
+      cat_id = page.content.cat_id
+    }
+
+    return fetch_topic(state, emitter, page, cat_id)
+
     emitter.emit('render')
   })
 
