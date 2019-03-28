35cec25a056b1534c89c59587cf970c14ac14a99
afincato <work@andrefincato.info>
2019-03-28 15:56:07 +0100

    make HKW page work again

--- app/stores/forum.js
+++ app/stores/forum.js
@@ -12,15 +12,17 @@ function forum (state, emitter) {
     let page
     let cat_id
 
-    if (state.route !== '/') {
-      page = ov(state.content).filter(page => page.uri === state.route)[0]
-      cat_id = page.content.cat_id
-    } else {
-      page = ov(state.content).filter(page => page.content.status === 'current')[0]
-      cat_id = page.content.cat_id
-    }
+    if (state.href !== '/hkw') {
+      if (state.route !== '/') {
+        page = ov(state.content).filter(page => page.uri === state.route)[0]
+        cat_id = page.content.cat_id
+      } else {
+        page = ov(state.content).filter(page => page.content.status === 'current')[0]
+        cat_id = page.content.cat_id
+      }
 
-    return fetch_topic(state, emitter, page, cat_id)
+      return fetch_topic(state, emitter, page, cat_id)
+    }
   })
 
   emitter.on('log-in', () => {
