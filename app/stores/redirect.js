c43fc055785387b2ef2a4d3ecd2c5f3abb7d0a02
afincato <work@andrefincato.info>
2019-09-26 17:32:15 +0200

    take out page redirect to current module when logged-in

--- app/stores/redirect.js
+++ app/stores/redirect.js
@@ -3,9 +3,6 @@ const ov = require('object-values')
 function redirect (state, emitter) {
   emitter.on('DOMContentLoaded', () => {
     if (state.route === '/') {
-      const page = ov(state.content).filter(page => page.content.status === 'current')[0]
-
-
       if (localStorage.getItem('user_data') !== undefined && localStorage.getItem('user_login') === 'true') {
         emitter.emit('pushState', page.uri)
       }
