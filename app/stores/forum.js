314bc676d87410bccecc1d9a11cc80e1df7e1a9e
afincato <work@andrefincato.info>
2019-10-07 18:59:20 +0200

    add VU module-03 only redirects

--- app/stores/forum.js
+++ app/stores/forum.js
@@ -6,7 +6,9 @@ function forum (state, emitter) {
     let page
     let cat_id
 
-    if (state.href !== '/hkw') {
+    const user_data = JSON.parse(localStorage.getItem('user_data'))
+
+    if (state.href !== '/hkw' && user_data.user.username !== 'vu' && localStorage.getItem('user_login') === 'false') {
       if (state.route !== '/') {
         page = ov(state.content).filter(page => page.uri === state.route)[0]
         cat_id = page.content.cat_id
@@ -14,20 +16,31 @@ function forum (state, emitter) {
         page = ov(state.content).filter(page => page.content.status === 'current')[0]
         cat_id = page.content.cat_id
       }
-
-      return fetch_topic(state, emitter, page, cat_id)
+    } else if (user_data.user.username === 'vu') {
+      page = ov(state.content).filter(page => page.uid === 'module-03')[0]
+      cat_id = page.content.cat_id
     }
+
+    return fetch_topic(state, emitter, page, cat_id)
   })
 
   emitter.on('log-in', () => {
     let page
     let cat_id
 
-    if (state.route !== '/') {
-      page = ov(state.content).filter(page => page.uri === state.route)[0]
-      cat_id = page.content.cat_id
-    } else {
-      page = ov(state.content).filter(page => page.content.status === 'current')[0]
+    const user_data = JSON.parse(localStorage.getItem('user_data'))
+
+    if (user_data.user.username !== 'vu' && localStorage.getItem('user_login') === 'false') { 
+      if (state.route !== '/') {
+        page = ov(state.content).filter(page => page.uri === state.route)[0]
+        cat_id = page.content.cat_id
+      } else {
+        page = ov(state.content).filter(page => page.content.status === 'current')[0]
+        cat_id = page.content.cat_id
+      }
+    } else if (user_data.user.username === 'vu') {
+      console.log('vu vu vu')
+      page = ov(state.content).filter(page => page.uid === 'module-03')[0]
       cat_id = page.content.cat_id
     }
 
--- app/stores/redirect.js
+++ app/stores/redirect.js
@@ -2,7 +2,30 @@ const ov = require('object-values')
 
 function redirect (state, emitter) {
   emitter.on('DOMContentLoaded', () => {
-    if (state.route !== '/') {
+    const user_data = JSON.parse(localStorage.getItem('user_data'))
+
+    if (user_data.user.username === 'vu' && localStorage.getItem('user_login') === 'true') {
+      if (state.route !== '/') {
+        emitter.emit('pushState', '/course/module-03')
+        emitter.emit('render')
+      }
+    } else if (state.route !== '/') {
+      if (localStorage.getItem('user_data') === null && localStorage.getItem('user_login') === 'false') {
+        emitter.emit('pushState', '/')
+        emitter.emit('render')
+      }
+    }
+  })
+
+  emitter.on('log-in', () => {
+    const user_data = JSON.parse(localStorage.getItem('user_data'))
+
+    if (user_data.user.username === 'vu' && localStorage.getItem('user_login') === 'true') {
+      if (state.route !== '/') {
+        emitter.emit('pushState', '/course/module-03')
+        emitter.emit('render')
+      }
+    } else if (state.route !== '/') {
       if (localStorage.getItem('user_data') === null && localStorage.getItem('user_login') === 'false') {
         emitter.emit('pushState', '/')
         emitter.emit('render')
