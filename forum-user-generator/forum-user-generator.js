596fab9fdcf11475ef9ab63811a65a80cab7f3a4
afincato <work@andrefincato.info>
2019-10-13 22:53:03 +0200

    sketch out fully user-creation process

--- forum-user-generator/forum-user-generator.js
+++ forum-user-generator/forum-user-generator.js
@@ -1,6 +1,11 @@
 require('dotenv').config()
 const fetch = require('node-fetch')
 const args = process.argv
+const pw_gen = require('generate-password')
+
+// USAGE:
+// node forum-user-generator.js <interest hash code>
+// lookup the interest code by using mailchimp playground APIs
 
 // example of Mailchimp API GET call
 // curl --request GET --url 'https://<string after dash of api key>.api.mailchimp.com/3.0/lists/<list id>/members/<lowercase md5 of email address>' --user '<anystring>:<api key>'
@@ -11,17 +16,95 @@ const authorization = Buffer.from([
 
 function forum_user_generator () {
   // get list of users from group
-  fetch(`https://${process.env.API_ID}.api.mailchimp.com/3.0/lists/${process.env.LIST_ID}/members?offset=0&count=200`, {
-    method: 'GET',
-    headers: {
-      Authorization: `Basic ${authorization}`
-    }
-  })
-    .then(response => response.json())
+
+  async function getMembers () {
+    let response = await fetch(`https://${process.env.API_ID}.api.mailchimp.com/3.0/lists/${process.env.LIST_ID}/members?offset=0&count=200`, {
+      method: 'GET',
+      headers: {
+        Authorization: `Basic ${authorization}`
+      }
+    })
+    return response.json()
+  }
+
+  let users = []
+  function processMembers (data, users) {
+    const members = data.filter(member => member.interests[args[2]] === true)
+
+    members.map(member => {
+      const usr = member.merge_fields.MMERGE1.replace(' ', '.').toLowerCase()
+      const pw = pw_gen.generate({ length: 19, numbers: false }).match(/.{1,7}/g).join('-')
+
+      const user = {
+        name: member.merge_fields.MMERGE1,
+        username: usr,
+        email: member.email_address,
+        password: pw,
+        active: true,
+        approved: true
+      }
+
+      users.push(user)
+    })
+    return users
+  }
+
+  async function createForumUser (data) {
+    let response = await fetch(`https://forum.englishes-mooc.org/users?api_key=${process.env.API_KEY_S}&api_username=${process.env.API_USR_S}`, {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(data)
+    })
+    return response.json()
+  }
+
+  async function deactivateForumUser (id) {
+    let response = await fetch(`https://forum.englishes-mooc.org/admin/users/${id}/deactivate.json?api_key=${process.env.API_KEY_S}&api_username=${process.env.API_USR_S}`, {
+      method: 'PUT'
+    })
+    return response
+  }
+
+  async function reactivateForumUser (id) {
+    let response = await fetch(`https://forum.englishes-mooc.org/admin/users/${id}/activate.json?api_key=${process.env.API_KEY_S}&api_username=${process.env.API_USR_S}`, {
+      method: 'PUT'
+    })
+    return response
+  }
+
+  getMembers()
     .then(response => {
-      const members = response.members.filter(member => member.interests[args[2]] === true)
-      members.map(member => {
-        console.log(member.email_address)
+      let data = processMembers(response.members, users)
+      return data
+    }).then(data => {
+      const ud = {
+        name: 'Masa',
+        username: 'masa',
+        email: 'masa@gva.de',
+        password: 'xx-yyyy-a-tt--c',
+        active: true,
+        approved: true
+      }
+
+      createForumUser(ud).then(response => {
+        console.log(response)
+
+        let id = response.user_id
+        deactivateForumUser(id)
+          .then(response => {
+            console.log('deactive-forum-user: status', response.statusText)
+            if (response.status === 200) {
+              reactivateForumUser(id)
+                .then(response => {
+                  console.log('reactive-forum-user: status', response.statusText)
+                })
+            }
+          })
+      }).catch(e => {
+        throw e
       })
     })
 }
