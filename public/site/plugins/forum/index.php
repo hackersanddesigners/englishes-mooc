bfdc5c00033207977aad476402aaf6317b39557e
afincato <work@andrefincato.info>
2020-04-26 16:06:54 +0200

    update user-login call

--- public/site/plugins/forum/index.php
+++ public/site/plugins/forum/index.php
@@ -76,12 +76,25 @@ Kirby::plugin('mooc/forum', [
         $usr = page('forum-users')->users()->toStructure()->findBy('user', $usr_name);
         $result = [];
 
+        // curl -X POST https://forum.englishes-mooc.org/session? \
+        // --data "login=cycle.four&password=rabid-proofs-underarm-hubcap-cheating-heftiness-gilled" \
+        // -H "Content-Type: multipart/form-data;" \
+        // -H "Api-Key: <key>" \
+        // -H "Api-Username: <usr_name>"
+
         if ($usr !== null) {
           $usr_key = $usr->key();
           $url_root = 'https://forum.englishes-mooc.org/';
-          $url = $url_root . 'session?api_key=' . $usr_key . '&api_username=' . $usr_name . '&login=' . $usr_name . '&password=' . $usr_pw;
+          $url = $url_root . 'session?login=' . $usr_name . '&password=' . $usr_pw;
 
-          $request = Remote::post($url);
+          $request = Remote::post($url, [
+            'headers' => [
+              "Content-Type: multipart/form-data;",
+              "Api-Key: $usr_key",
+              "Api-Username: $usr_name"
+			      ]
+          ]);
+
           $result = $request->json();
         } else {
           $result = ['error' => 'Check your username and / or password'];
